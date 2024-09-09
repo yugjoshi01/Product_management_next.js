10b2feab7e17716a876e7e2adf8e5f5e4143e7c0
Yug Joshi <yugjoshi628@gmail.com>
2024-09-09 11:38:10 +0530

    vhj

---
+++ src/components/Ui_layout.jsx
@@ -0,0 +1,93 @@
+"use client"
+import {
+    DesktopOutlined,
+    PieChartOutlined
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children, onClick) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    onClick, 
+  };
+}
+
+const handleClick = (menuItemKey) => {
+    switch (menuItemKey) {
+      case "1":
+        console.log("Products clicked");
+        break;
+      case "2":
+        console.log("Yug clicked");
+        break;
+      default:
+        break;
+    }
+  };
+const items = [
+  getItem('Products', '1', <PieChartOutlined />,() => handleClick("1")),
+  getItem('yug', '2', <DesktopOutlined />,() => handleClick("2")),
+
+];
+const Uilayout = ({children}) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+           {children}
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default Uilayout;
